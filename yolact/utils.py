9d4af36f5cfd880d1a3cb353a3fed7f7cb803bb8
claylau <56223538+claylau@users.noreply.github.com>
2020-03-07 11:42:52 +0800

    Create utils.py

---
+++ yolact/utils.py
@@ -0,0 +1,94 @@
+# copy from original yolact and detectron2
+
+import torch
+from detectron2.layers import cat
+
+
+def sanitize_coordinates(_x1, _x2, img_size:int, padding:int=0, cast:bool=True):
+    """
+    Sanitizes the input coordinates so that x1 < x2, x1 != x2, x1 >= 0, and x2 <= image_size.
+    Also converts from relative to absolute coordinates and casts the results to long tensors.
+
+    If cast is false, the result won't be cast to longs.
+    Warning: this does things in-place behind the scenes so copy if necessary.
+    """
+    _x1 = _x1 * img_size
+    _x2 = _x2 * img_size
+    if cast:
+        _x1 = _x1.long()
+        _x2 = _x2.long()
+    x1 = torch.min(_x1, _x2)
+    x2 = torch.max(_x1, _x2)
+    x1 = torch.clamp(x1-padding, min=0)
+    x2 = torch.clamp(x2+padding, max=img_size)
+
+    return x1, x2
+
+
+def crop(masks, boxes, padding:int=1):
+    """
+    "Crop" predicted masks by zeroing out everything not in the predicted bbox.
+    Vectorized by Chong (thanks Chong).
+
+    Args:
+        - masks should be a size [h, w, n] tensor of masks
+        - boxes should be a size [n, 4] tensor of bbox coords in relative point form
+    """
+    h, w, n = masks.size()
+    x1, x2 = sanitize_coordinates(boxes[:, 0], boxes[:, 2], w, padding, cast=False)
+    y1, y2 = sanitize_coordinates(boxes[:, 1], boxes[:, 3], h, padding, cast=False)
+
+    rows = torch.arange(w, device=masks.device, dtype=x1.dtype).view(1, -1, 1).expand(h, w, n)
+    cols = torch.arange(h, device=masks.device, dtype=x1.dtype).view(-1, 1, 1).expand(h, w, n)
+    
+    masks_left  = rows >= x1.view(1, 1, -1)
+    masks_right = rows <  x2.view(1, 1, -1)
+    masks_up    = cols >= y1.view(1, 1, -1)
+    masks_down  = cols <  y2.view(1, 1, -1)
+    
+    crop_mask = masks_left * masks_right * masks_up * masks_down
+    
+    return masks * crop_mask.float()
+
+
+def mask_iou(mask1, mask2):
+    intersection = torch.sum(mask1*mask2, dim=(0, 1))
+    area1 = torch.sum(mask1, dim=(0, 1))
+    area2 = torch.sum(mask2, dim=(0, 1))
+    union = (area1 + area2) - intersection
+    ret = intersection / union
+    return ret
+
+
+def permute_to_N_HWA_K(tensor, K):
+    """
+    Transpose/reshape a tensor from (N, (A x K), H, W) to (N, (HxWxA), K)
+    """
+    assert tensor.dim() == 4, tensor.shape
+    N, _, H, W = tensor.shape
+    tensor = tensor.view(N, -1, K, H, W)
+    tensor = tensor.permute(0, 3, 4, 1, 2)
+    tensor = tensor.reshape(N, -1, K)  # Size=(N,HWA,K)
+    return tensor
+
+
+def permute_all_cls_and_box_to_N_HWA_K_and_concat(box_cls, box_delta, num_classes=80):
+    """
+    Rearrange the tensor layout from the network output, i.e.:
+    list[Tensor]: #lvl tensors of shape (N, A x K, Hi, Wi)
+    to per-image predictions, i.e.:
+    Tensor: of shape (N x sum(Hi x Wi x A), K)
+    """
+    # for each feature level, permute the outputs to make them be in the
+    # same format as the labels. Note that the labels are computed for
+    # all feature levels concatenated, so we keep the same representation
+    # for the objectness and the box_delta
+    box_cls_flattened = [permute_to_N_HWA_K(x, num_classes) for x in box_cls]
+    box_delta_flattened = [permute_to_N_HWA_K(x, 4) for x in box_delta]
+    # concatenate on the first dimension (representing the feature levels), to
+    # take into account the way the labels were generated (with all feature maps
+    # being concatenated as well)
+    box_cls = cat(box_cls_flattened, dim=1).view(-1, num_classes)
+    box_delta = cat(box_delta_flattened, dim=1).view(-1, 4)
+    return box_cls, box_delta
+
